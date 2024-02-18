d3832ea21d8a12188134a06b40066a991150743c
Gaetan Lepage <gaetan@glepage.com>
2024-02-18 15:01:54 +0100

    switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -5,33 +5,44 @@
       url = "github:moergo-sc/zmk";
       flake = false;
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    glove80-zmk
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    packages."x86_64-linux".default = let
-      firmware = import glove80-zmk {inherit pkgs;};
+    glove80-zmk,
+    flake-parts,
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-      keymap = ./glove80.keymap;
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages.default = let
+          firmware = import glove80-zmk {inherit pkgs;};
 
-      glove80_left = firmware.zmk.override {
-        board = "glove80_lh";
-        inherit keymap;
+          keymap = ./glove80.keymap;
+
+          glove80_left = firmware.zmk.override {
+            board = "glove80_lh";
+            inherit keymap;
+          };
+
+          glove80_right = firmware.zmk.override {
+            board = "glove80_rh";
+            inherit keymap;
+          };
+        in
+          firmware.combine_uf2 glove80_left glove80_right;
+
+        formatter = pkgs.alejandra;
       };
-
-      glove80_right = firmware.zmk.override {
-        board = "glove80_rh";
-        inherit keymap;
-      };
-    in
-      firmware.combine_uf2 glove80_left glove80_right;
-
-    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
-  };
+    };
 }
